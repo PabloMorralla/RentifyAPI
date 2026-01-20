1d4008a5155e0818ba1fae97de355f2bcbd97ff7
guicamher <guillecampher15@gmail.com>
2026-01-20 19:57:35 +0100

    test

--- main.py
+++ main.py
@@ -368,6 +368,30 @@ def get_user_by_owner(owner_fk: int):
         }
 
 
+
+
+def get_user_by_id(user_id: int):
+
+    if not user_id:
+        raise HTTPException(status_code=400, detail="ID obligatorio")
+
+    query = """SELECT id, first_name, last_name, phone_number, email
+            FROM Users
+            WHERE id = ?"""
+
+    user = execute_query(query, [user_id])[0]
+    print(user)
+    return {
+            "id": user[0],
+            "first_name": user[1],
+            "last_name": user[2],
+            "phone_number": user[3],
+            "email": user[4],
+
+        }
+
+
+
 # -------------
 # PUT /update/user
 # -------------
@@ -464,11 +488,10 @@ def update_user(
         conn.commit()
 
         if cursor.rowcount == 0:
-            return {"message": "No se actualizó ningún registro"}
+            return {"message": "No se actualizó ningún usuario"}
         else:
-            return {
-                "id": user.id
-            }
+            return get_user_by_id(body["id"])
+
 
     except sqlite3.IntegrityError as e:
         if conn:
@@ -488,7 +511,6 @@ def update_user(
     finally:
         if conn:
             conn.close()
-
 
 
 
@@ -514,6 +536,7 @@ def delete_user(user_id: int):
         "message": "Usuario eliminado correctamente",
         "user_id": user_id
     }
+
 
 
 
@@ -567,5 +590,4 @@ def create_incident(
     finally:
         if conn:
             conn.close()
-
 
